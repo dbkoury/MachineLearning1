96852f64f263acf03e483fbd43418860ce186c26
dbkoury <dbkoury@email.wm.edu>
2021-10-12 17:11:39 -0400

    Update Case2.R

--- Case2.R
+++ Case2.R
@@ -5,9 +5,9 @@
 options(scipen=999)
 
 #Getting the Data
-setwd("C:/Users/dylan/MSBA/ML1/Assignments")
+#setwd("C:/Users/dylan/MSBA/ML1/Assignments")
 Case2 <- read.csv("Case2.csv")
-
+attach(Case2)
 #Looking at the Data
 str(Case2) 
 #looking at the structure of our data we can see most of the variables are integers, 
@@ -46,13 +46,38 @@ cor(Case2[,c(-1,-2)], use="pairwise")
   #Given the correlation of Tenured was about .5 (positive, moderate) with FTRetentionRate, 
   #we should expect to see the model to have a positive relationship between these variables as well
 #actually create model
+lmTenure <- lm(FTRetentionRate ~ Tenured, data=Case2)
 #discuss model (R^2, p-value, slope)
+summary(lmTenure)
+#Equation: FTRetentionRate = 73.5027636 + 0.0178277 * Tenured
+  #So there is a positive relationship between these variables, with Retention increasing by 0.0178 percentage points for every tenured professor
+  #And the intercept means for universities with no tenured professors the model predicts a retention rate of 73.5%
+  #It is important to note that the slope and intercept both have significant p-values at <0.0000000000000002 ***
+  #lmTenure also tells us that 738 observations were deleted due to missingness
+  #Our residual standard error is 9.295
+  #Lastly our R-sq value for the model is .2248, 
+  #meaning that this model (Tenured) is able to explain 22.5% of the variation in the data (FTRetentionRate)
 #describe visually with plots of model and regression
 #Test Assumptions
-#Linearity - plot x and y
-#Normalized Residuals - Normal or Skewed
-#Homoskedasticity - BPTest
-#Remaining assumptions with plot of lm
+  #Linearity - plot x and y
+plot(Tenured, FTRetentionRate)
+abline(lmTenure, col = "red")
+    #While the line does capture a good amount of the data, 
+    #it is clear that most of the observations are clustered around 0-500 tenured professors,
+    #with a good number of observations quite far from the regression line
+    #this data would likely better be modeled with a log of Tenured
+  #Normalized Residuals - Normal or Skewed
+hist(lmTenure$residuals)
+mean(lmTenure$residuals)
+    #The histogram of the residuals is centered around zero (mean: -0.000000000000002219908),
+    #however it does appear slightly skewed left, 
+    #likely due to those early observations well below the regression line  we mentioned earlier
+  #Homoskedasticity - BPTest
+lmtest::bptest(FTRetentionRate ~ Tenured)
+    #The Breush-Pagan test gives us a p-value of 0.005616,
+    #meaning that the model is likely heteroskedastic, failing this assumption
+  #Remaining assumptions with plot of lm
+plot(lmTenure)
 #Explanation of model in context
     
 ###VARIABLE 3: Total Faculty
