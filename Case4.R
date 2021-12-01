06e8229839902f47411c22bf0eb92b095ac6d280
dbkoury <dbkoury@email.wm.edu>
2021-12-01 18:41:54 -0500

    Update Case4.R

--- Case4.R
+++ Case4.R
@@ -28,6 +28,47 @@ test <- data[-divideData,]
 
 ##Logistic Model
 logisticmodel <- glm(intubated~.,data=train, family=binomial)
+
+##Assumptions
+#Linearity of the Logit
+  #The only continuous variable is age
+plot(data$age,log(data$age))
+interaction <- data$age*log(data$age)
+linearitylog <- glm(intubated~interaction, family=binomial,data=data)
+summary(linearitylog)
+#Looking at the graph of the age variable against its log values,
+#As well as the significance of the interaction term between them when modeled with intubation,
+#We can see that the inclusion of the age variable would violate our assumptions
+
+#Multicollinearity
+#car::vif(logisticmodel)
+#We produced an error with this, meaning it may be likely we have perfect multicollineaity
+summary(c(data$Gender, data$pregnant))
+#A closer look at the data and it seems that the "Gender" and "Pregnant" variables may be the issue,
+#as the "gender" variable is redundant as every male observation is "Not Applicable" under the "Pregnant" variable
+#Let's rerun the model without Gender and then test again for multicollinearity
+logisticmodel <- glm(intubated~.-Gender,data=train, family=binomial)
+#car::vif(logisticmodel)
+#The model still would not run meaning there is still collinearity somewhere.
+#Looking ahead, you will see later that we had trouble getting our QDA function to run,
+#this was due to rank deficiency, or rather multicollinearity. 
+#We were able to discover what other variables were multicollinear with a rather rudimentary method
+#We just plugged in variables into the model to see which ones broke it.
+#Interestingly we found the two other variables that could not coexist were 
+#"LabResults" and "finalClassification"
+#Looking back the reason was rather intuitive, similarly to the prior issue,
+#final classification is just a more broken down version of the lab results.
+#Looking at the summary
+summary(c(data$labResults, data$finalClassification))
+#You can see that the "No" category is the same as the "Negative Case" category.
+#We ultimately chose to stick with the "finalClassification" as it is more fine data with more categories
+#as well as an updated ideally more accurate version of the original "labResults"
+#Including both would also violate our assumption of Independence of Errors,
+#As having a second covid test is a repeated measurement.
+logisticmodel <- glm(intubated~.-Gender-labResults,data=train, family=binomial)
+car::vif(logisticmodel)
+#And now as you can see our vif test runs and all the variables look good
+
 probs <- predict(logisticmodel, type="response", newdata=test)
 pred <- ifelse(probs>0.5, "Yes", "No")
 (logtable <- table(pred,test$intubated,dnn=c("Predicted","Observed")))
@@ -68,7 +109,7 @@ preprocessing <- train %>% preProcess(method=c("center","scale"))
 traintransformed <- preprocessing %>% predict(train)
 testtransformed <- preprocessing %>% predict(test)
 
-ldamodel <- lda(intubated~.,data=traintransformed)
+ldamodel <- lda(intubated~.-Gender-labResults,data=traintransformed)
 
 predictions <- ldamodel %>% predict(testtransformed)
 (ldatable <- table(predictions$class,testtransformed$intubated, dnn=c("Predicted","Observed")))
@@ -87,21 +128,22 @@ predictions <- ldamodel %>% predict(testtransformed)
 (ldaB <- ldatable[3]) #10322
 #False Negatives
 (ldaC <- ldatable[2]) #1
-
+#Sensitivity
 (ldaSensitivity <- ldaA/(ldaA+ldaC)) #0.9999785
-
+#Specificity
 (ldaSpecificity <- ldaD/(ldaB+ldaD)) #0.00009687106
 
-#Sensitivity
-#Specificity
 #Balanced Accuracy
 (ldaBalanced <- (ldaSensitivity+ldaSpecificity)/2)
 #Our balanced accuracy is 0.5000377
 
 #QDA Model
 #qdamodel <- qda(intubated~.,data=traintransformed)
-  #This model would not run for us
-  #INSERT RATIONALE
+  #This model would not run for us, as there appears to be a rank deficiency in the "No" group
+#As discussed earlier, we had to edit the model to get it to run properly
+qdamodel <- qda(intubated~.-Gender-labResults,data=traintransformed)
+
+
 
 #KNN Model
 set.seed(666)
