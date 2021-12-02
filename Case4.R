85413ab5e5d3bffd2840e29207e45fada6c8eac5
dbkoury <dbkoury@email.wm.edu>
2021-12-01 19:27:05 -0500

    Update Case4.R

--- Case4.R
+++ Case4.R
@@ -29,47 +29,8 @@ test <- data[-divideData,]
 ##Logistic Model
 logisticmodel <- glm(intubated~.,data=train, family=binomial)
 
-##Assumptions
-#Linearity of the Logit
-  #The only continuous variable is age
-plot(data$age,log(data$age))
-interaction <- data$age*log(data$age)
-linearitylog <- glm(intubated~interaction, family=binomial,data=data)
-summary(linearitylog)
-#Looking at the graph of the age variable against its log values,
-#As well as the significance of the interaction term between them when modeled with intubation,
-#We can see that the inclusion of the age variable would violate our assumptions
-
-#Multicollinearity
-#car::vif(logisticmodel)
-#We produced an error with this, meaning it may be likely we have perfect multicollineaity
-summary(c(data$Gender, data$pregnant))
-#A closer look at the data and it seems that the "Gender" and "Pregnant" variables may be the issue,
-#as the "gender" variable is redundant as every male observation is "Not Applicable" under the "Pregnant" variable
-#Let's rerun the model without Gender and then test again for multicollinearity
-logisticmodel <- glm(intubated~.-Gender,data=train, family=binomial)
-#car::vif(logisticmodel)
-#The model still would not run meaning there is still collinearity somewhere.
-#Looking ahead, you will see later that we had trouble getting our QDA function to run,
-#this was due to rank deficiency, or rather multicollinearity. 
-#We were able to discover what other variables were multicollinear with a rather rudimentary method
-#We just plugged in variables into the model to see which ones broke it.
-#Interestingly we found the two other variables that could not coexist were 
-#"LabResults" and "finalClassification"
-#Looking back the reason was rather intuitive, similarly to the prior issue,
-#final classification is just a more broken down version of the lab results.
-#Looking at the summary
-summary(c(data$labResults, data$finalClassification))
-#You can see that the "No" category is the same as the "Negative Case" category.
-#We ultimately chose to stick with the "finalClassification" as it is more fine data with more categories
-#as well as an updated ideally more accurate version of the original "labResults"
-#Including both would also violate our assumption of Independence of Errors,
-#As having a second covid test is a repeated measurement.
-logisticmodel <- glm(intubated~.-Gender-labResults,data=train, family=binomial)
-car::vif(logisticmodel)
-#And now as you can see our vif test runs and all the variables look good
-
 probs <- predict(logisticmodel, type="response", newdata=test)
+#NOTE: Although our model ran, a warning message was presented informing us that the fit was rank-deficient
 pred <- ifelse(probs>0.5, "Yes", "No")
 (logtable <- table(pred,test$intubated,dnn=c("Predicted","Observed")))
 (logaccuracy <- mean(pred==test$intubated))
@@ -139,34 +100,40 @@ predictions <- ldamodel %>% predict(testtransformed)
 
 #QDA Model
 #qdamodel <- qda(intubated~.,data=traintransformed)
-  #This model would not run for us, as there appears to be a rank deficiency in the "No" group
-#As discussed earlier, we had to edit the model to get it to run properly
+  #This model would not run for us, as the error message informs us of  a rank deficiency in the "No" group
+#This was foreshadowed by our previous error message in the logistic model
+
+#Rank deficiency is "when one or more of the independent variables are a linear function of the other independent variables in the model."
+#i.e., multicollinearity, which is one of our assumptions. In order to test for this we conducted a vif-test on our logistic model
+#car::vif(logisticmodel)
+#We produced an error with this, meaning it may be likely we have perfect multicollineaity
+summary(c(data$Gender, data$pregnant))
+#A closer look at the data and it seems that the "Gender" and "Pregnant" variables may be the issue,
+#as the "gender" variable is redundant as every male observation is "Not Applicable" under the "Pregnant" variable, meaning they have the same value
+#Let's rerun the model without Gender and then test again for multicollinearity
+logisticmodel2 <- glm(intubated~.-Gender,data=train, family=binomial)
+#car::vif(logisticmodel2)
+#The model still would not run meaning there is still collinearity somewhere.
+#We were able to discover what other variables were multicollinear with a rather rudimentary method
+#We just plugged in variables into the QDA model to see which ones broke it.
+#Interestingly we found the two other variables that could not coexist were 
+#"LabResults" and "finalClassification"
+#Looking back the reason was rather intuitive, similarly to the prior issue,
+#final classification is just a more broken down version of the lab results.
+#Looking at the summary
+summary(c(data$labResults, data$finalClassification))
+#You can see that the "No" category is the same as the "Negative Case" category.
+#We ultimately decided should we have dropped a variable, to run the model
+#sticking with the "finalClassification" would be the best course of action
+#as it is more fine data with more categories
+#as well as an updated ideally more accurate version of the original "labResults"
+#Including both would also violate our assumption of Independence of Errors,
+#As having a second covid test is a repeated measurement.
+logisticmodel <- glm(intubated~.-Gender-labResults,data=train, family=binomial)
+car::vif(logisticmodel)
+#And now as you can see our vif test runs and all the variables look good
 qdamodel <- qda(intubated~.-Gender-labResults,data=traintransformed)
-
-qdapredictions <- qdamodel %>% predict(testtransformed)
-#Accuracy Rate - 
-(qdaaccuracy <- mean(qdapredictions$class == testtransformed$intubated))
-#Error Rate - 
-(qdaerror <- 1-qdaaccuracy)
-# QDA model actually did worse with a 0.8577778 accuracy rate given proportion of .8 and set seed of 1000 - likely overfitting the data
-(qdatable <- table(predictions$class,testtransformed$intubated, dnn=c("Predicted","Observed")))
-
-#True Positives
-(qdaA <- qdatable[1]) #
-#True Negatives
-(qdaD <- qdatable[4]) #
-#False Positives
-(qdaB <- qdatable[3]) #
-#False Negatives
-(qdaC <- qdatable[2]) #
-#Sensitivity
-(qdaSensitivity <- qdaA/(qdaA+qdaC)) #
-#Specificity
-(qdaSpecificity <- qdaD/(qdaB+qdaD)) #
-
-#Balanced Accuracy
-(qdaBalanced <- (qdaSensitivity+qdaSpecificity)/2)
-#Our balanced accuracy is 
+#In fact when we run the qdamodel without these variables the model runs without issue.
 
 #KNN Model
 set.seed(666)
